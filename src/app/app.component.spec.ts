c94a864cb382493866d7ce4fbffc22f3fbfcb907
Christian Omlin <christian.omlin@siemens.com>
2023-01-28 14:41:39 +0100

    chore: reset project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'firebase-pg'`, () => {
+  it(`should have as title 'firebase-ng'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('firebase-pg');
+    expect(app.title).toEqual('firebase-ng');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('firebase-pg app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('firebase-ng app is running!');
   });
 });
