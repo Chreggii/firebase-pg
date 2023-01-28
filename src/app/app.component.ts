6671934d61e695bec3623c8c036324e95a84a6c9
Christian Omlin <christian.omlin@siemens.com>
2023-01-28 17:25:36 +0100

    chore: udpate gitignore

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,10 @@
 import { Component } from '@angular/core';
 
-import { getFirebaseSetting } from './app.module';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  constructor() {
-    console.log(getFirebaseSetting);
-  }
-
   title = 'firebase-pg';
 }
